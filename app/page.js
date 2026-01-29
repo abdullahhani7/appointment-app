7bad48353be31aa3f270e86a81cf728b4e8c8a59
Abdullah <abdulhhany69@gmail.com>
2026-01-29 07:10:11 +0200

    Add Header component; update button styles and remove global styles

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
-      Hello Next
+     <Button  >Button</Button>
     </div>
   );
 }
