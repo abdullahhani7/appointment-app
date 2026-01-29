ac79616916a0a1c06060135e01a71fc6dc8df57b
Abdullah <abdulhhany69@gmail.com>
2026-01-29 08:01:45 +0200

    Add Hero component with responsive layout and call-to-action button; update global styles and layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
+
 import "./globals.css";
 import Header from "./_components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-roboto",
 });
 
 export const metadata = {
@@ -20,9 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${roboto.variable}   antialiased`}>
         <div className="md:px-20  shadow-sm">
           <Header />
         </div>
