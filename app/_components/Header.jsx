7bff9ca5b0c452e0deb80916ae7223b5829321b5
Abdullah <abdulhhany69@gmail.com>
2026-01-29 07:44:23 +0200

    Add Header component with navigation menu and logo; update layout and home page structure

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const menu = [
+    {
+      id: 1,
+      name: "Home",
+      path: "/",
+    },
+    {
+      id: 2,
+      name: "Explore",
+      path: "/explore",
+    },
+    {
+      id: 3,
+      name: "Contact Us",
+      path: "/contact-us",
+    },
+  ];
 
-export default Header
+  return (
+    <header className="px-4">
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-10">
+          <Image
+            src="/assets/images/logo.png"
+            width={100}
+            height={100}
+            alt="logo"
+          />
+          <ul className="md:flex gap-8 hidden">
+            {menu.map((item) => (
+              <Link key={item.id} href={item.path}>
+                <li className="hover:text-lime-600 cursor-pointer hover:scale-105 transition-all">
+                  {item.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <Button>Get Started</Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <div className="md:px-20  shadow-sm">
+          <Header />
+        </div>
         {children}
       </body>
     </html>
