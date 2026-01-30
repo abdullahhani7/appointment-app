736bf8fc6180910d36e7f4c741031bbf0b4d4697
Abdullah <abdulhhany69@gmail.com>
2026-01-30 11:38:15 +0200

    feat: refactor search page to use useParams and improve data fetching logic

--- app/(route)/search/[cname]/page.js
+++ app/(route)/search/[cname]/page.js
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
 
-const page = async ({ params }) => {
-  const { cname } = await params;
+
+import { getDoctorsByCategory } from "@/app/_utils/Api";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const page = ( ) => {
+  // const { cname } = React.use(params);
+  const params = useParams();
+  const { cname } = params;
+
+  const [doctorsByCategoryList, setDoctorsByCategoryList] = useState([]);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      const doctors = await getDoctorsByCategory(cname);
+      console.log("doctorsByCategoryList: ", doctors);
+      setDoctorsByCategoryList(doctors);
+    };
+    fetchDoctors();
+  }, [cname]);
 
   console.log(cname);
 
