df0a749a6ea349584a21e64a576793d651f74e8d
Abdullah <abdulhhany69@gmail.com>
2026-01-31 08:24:34 +0200

    feat: enhance BookAppointment component with time slot calculation and selection

--- app/(route)/details/BookAppointment.jsx
+++ app/(route)/details/BookAppointment.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Calendar } from "@/components/ui/calendar";
 import {
   Dialog,
@@ -12,6 +12,46 @@ import { Button } from "@/components/ui/button";
 
 const BookAppointment = () => {
   const [date, setDate] = useState(new Date());
+  const [timeSlot, setTimeSlot] = useState();
+  const [selectedTime, setSelectedTime] = useState();
+
+  const pastDay = (day) => {
+    return day <= new Date();
+  };
+
+  const now = new Date();
+console.log("now",now);
+
+  const timeList = [];
+
+  useEffect(() => {
+    calcTime();
+    console.log("timeList", timeList);
+  }, []);
+
+  const calcTime = () => {
+    for (let i = 10; i <= 12; i++) {
+      timeList.push({
+        time: i + ":00",
+      });
+
+      timeList.push({
+        time: i + ":30",
+      });
+    }
+
+    for (let i = 1; i <= 5; i++) {
+      timeList.push({
+        time: i + ":00",
+      });
+
+      timeList.push({
+        time: i + ":30",
+      });
+    }
+
+    setTimeSlot(timeList);
+  };
 
   return (
     <Dialog>
@@ -21,19 +61,31 @@ const BookAppointment = () => {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Book Appointment</DialogTitle>
-          <DialogDescription>
-            <div>
-              <div>
-                <Calendar
-                  mode="single"
-                  selected={date}
-                  onSelect={setDate}
-                  className="rounded-lg border"
-                />
-              </div>
-              <div></div>
+          {/* <DialogDescription> */}
+          <div className="grid grid-cols-1 md:grid-cols-2">
+            <div className="flex">
+              <Calendar
+                mode="single"
+                selected={date}
+                onSelect={setDate}
+                className="rounded-md border"
+                disabled={pastDay}
+              />
             </div>
-          </DialogDescription>
+            <div className="mt-5 md:mt-0">
+              <div className="grid grid-cols-3 gap-3 border border-lg p-3">
+                {timeSlot?.map((item, index) => (
+                  <h2
+                    onClick={() => setSelectedTime(item.time)}
+                    className={`border text-center hover:bg-lime-300 hover:text-lime-800 cursor-pointer  p-2  rounded-full ${item.time == selectedTime && "bg-lime-300"}   `}
+                  >
+                    {item.time}
+                  </h2>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* </DialogDescription> */}
         </DialogHeader>
       </DialogContent>
     </Dialog>
